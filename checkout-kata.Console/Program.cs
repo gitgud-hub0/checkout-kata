9925056175e5b59b6d7dda8a3a4119ba33963e89
Haobin Qu <hq617@york.ac.uk>
2020-03-08 18:45:19 +0000

    initial commit

--- checkout-kata.Console/Program.cs
+++ checkout-kata.Console/Program.cs
@@ -6,7 +6,7 @@ namespace checkout_kata.Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World! Testing");
         }
     }
 }
